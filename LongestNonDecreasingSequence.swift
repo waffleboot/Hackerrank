05c28770bb0d26b7d98d9ae6b393a03a58b3b4bb
yangand <waffleboot@gmail.com>
2016-06-04 11:40:10 +0400

    N*logN

--- LongestNonDecreasingSequence.swift
+++ LongestNonDecreasingSequence.swift
@@ -15,3 +15,35 @@ func longestNonDecreasingSequence() {
 }
 
 longestNonDecreasingSequence()
+
+struct LIS {
+
+    var dp: [Int]
+    var longest = 0
+    let inf = 2000000000
+    
+    init(size: Int) {
+        self.dp = Array(count: size, repeatedValue: inf)
+    }
+    
+    mutating func add(x: Int) {
+        var left = 1
+        var right = longest
+        while left <= right {
+            let mid = left + ((right - left) >> 1)
+            if dp[mid] <= x {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+        right += 1
+        dp[right] = x
+        longest = max(right,longest)
+    }
+
+}
+
+var lis = LIS(size: S.count)
+S.forEach { lis.add($0) }
+print(lis.longest)
